b3fede4d9666e76f310f4e25c835edeb661e2b92
Juanfer245 <jfc.tipan@yavirac.edu.ec>
2022-12-07 14:24:33 -0500

    version 1.0

---
+++ templates-ALC-grupo4-master/src/app/pages/pages-routing.module.ts
@@ -0,0 +1,54 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
+import { PagesComponent } from './pages.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { ProductComponent } from './product/product.component';
+import { CatalogueComponent } from './catalogue/catalogue.component';
+import { StaticModule } from '../static/static.module';
+import { HomeComponent } from './home/home.component';
+import { CarritoComponent } from '../carrito/carrito.component';
+
+
+const routes: Routes = [
+
+  {
+    path: 'dashboard',
+    component: PagesComponent,
+    //Rutas hijas
+    children: [
+      {
+        path: '',
+        component: DashboardComponent,
+      },
+      {
+        path: 'product',
+        component: ProductComponent,
+      },
+      {
+        path: 'catalogue',
+        component: CatalogueComponent,
+      },
+      {
+        path: 'home',
+        component: HomeComponent,
+      },
+      {
+        path: 'carrito',
+        component: CarritoComponent,
+      },
+
+
+    ],
+  },
+]
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    StaticModule,
+    RouterModule.forChild(routes)
+  ]
+  //Forchild invoca a los hijos
+})
+export class PagesRoutingModule { }
